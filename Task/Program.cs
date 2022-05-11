a47b14c9e482710a396f8275589ffadddf926766
Ivan <mmavrin80@gmail.com>
2022-05-11 18:56:13 +0300

    Начало метода и заполнение временного массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,14 @@
 ﻿string[] array = {"hello", "2", "world", ":-)"};
+string[] NextArr (string[] arg)
+{
+    int size = 0;
+    string[] TempArray = new string[arg.Length];
+    for(int i = 0; i < arg.Length; i++)
+    {
+        if(arg[i].Length <= 3) 
+        {
+            size++;
+            TempArray[i] = arg[i];
+        }
+    }
+}
